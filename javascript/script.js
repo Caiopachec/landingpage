f8c25a9886ecda2ef49fff8d6c36e245591d78d6
Caiopachec <caiopacheco936@gmail.com>
2024-10-12 15:32:58 -0300

    mudanças2

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,50 @@
 $(document).ready(function() {
-    $('#mobile-btn').on('click', function() {
-        $('#mobile-menu').toggleClass('active');  // Corrigido para adicionar o #
-        $('#mobile-btn').find('i').toggleClass('fa-x');
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 
     const sections = $('section');
     const navItems = $('.nav-item');
 
     $(window).on('scroll', function () {
-      const header = $('header');
-      
-      const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
-      let activeSecctionIndex = 0;
+        let activeSectionIndex = 0;
 
-      if (scrollPosition<= 0 ) {
-        header.css('box-shadow', 'none');
-      }else {
-        header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.2)');            
-     }
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
 
-sections.each(function(i){
-    const section = $(this);
-    const sectionTop = section.offset().top - 96;
-    const sectionBottom = sectionTop+ section.outerHeight();
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
 
-    if (scrollPosition >= sectionTop && sectionPosition < sectionBottom){
-        activeSecctionIndex = i;
-        return false;
-    }
-})
-navItems.removeClass('active');
- $(navItems[activeSecctionIndex]).addClass('active');
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
 
-    })
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
 
-    scrollReveal().reveal('#cta', {
-        origin: 'left' ,
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
         duration: 2000,
         distance: '20%'
     });
 
-    scrollReveal().reveal('.dish', {
-        origin: 'left' ,
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
         duration: 2000,
         distance: '20%'
     });
+
 
 });
